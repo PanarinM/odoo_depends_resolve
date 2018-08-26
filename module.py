b95b3ffb7945b2a06271e28f5b738b33ec87d5c4
mpanarin <m.panarin@mobilunity.com>
2018-08-26 15:35:26 +0300

    add descriptor parsing

--- module.py
+++ module.py
@@ -1,6 +1,8 @@
 import os
+import ast
 
 from global_parameters import DESCRIPTOR_NAMES
+from custom_exceptions import DescriptorError
 
 
 class Module(object):
@@ -8,7 +10,7 @@ class Module(object):
     def __init__(self, path):
         self.path = path
         self._descriptor = self.__get_descriptor()
-        self.name, self.depends = self.__parse_descriptor()
+        self.name, self.depends, self.info_dict = self.__parse_descriptor()
         self.tech_name = self.__get_tech_name()
 
     def __get_descriptor(self):
@@ -20,14 +22,25 @@ class Module(object):
 
     def __parse_descriptor(self):
         with open(os.path.join(self.path, self._descriptor), "r") as descriptor:
-            pass
-        return '', ''
+            node = ast.parse(descriptor.read()).body
+            if len(node) != 1:
+                raise DescriptorError(
+                    f"Module with path {self.path} has incorrectly "
+                    "filled {self._descriptor}"
+                )
+            node = node[0].value
+            info_dict = ast.literal_eval(node)
+        return (
+            info_dict.get("name", ""),
+            info_dict.get("depends", []),
+            info_dict,
+        )
 
     def __get_tech_name(self):
-        return ''
+        return os.path.basename(self.path)
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
-        return f"Module({self.name}, {self.tech_name})"
+        return f"Odoo Module('{self.name}', '{self.tech_name}')"
